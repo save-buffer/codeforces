fa11447d856cb30e5c296a1a039c360a69ada196
save-buffer <warplane@hotmail.com>
2018-03-23 12:07:46 -0700

    Round 471

--- 471/A.cpp
+++ 471/A.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 
 int main(void)
-{    
+{
+    int h, m;
+    cin >> h >> m;
+
+    int H, D, C, N;
+    cin >> H >> D >> C >> N;
+
+    int cur_min = h * 60 + m;
+    int store = 20 * 60;
+
+    int min_to_discount = max(store - cur_min, 0);
+
+    float with_discount = 0.8f * ceil((((float)H + (float)D * min_to_discount) / (float)N)) * C;
+    float without_discount = ceil((float)H / (float)N) * C;
+
+    cout << min(with_discount, without_discount) << endl;
+    
     return(0);
 }
--- 471/B.cpp
+++ 471/B.cpp
@@ -3,6 +3,45 @@
 using namespace std;
 
 int main(void)
-{    
+{
+    string s;
+    cin >> s;
+
+    if(s.size() < 4)
+    {
+	cout << "No" << endl;
+	return(0);
+    }
+
+    int count[26];
+    for(int i = 0; i < 26; i++)
+	count[i] = 0;
+    for(char c : s)
+	count[c - 'a']++;
+
+    int types_of_chars = 0;
+    int min_count = INT_MAX;
+    for(int i = 0; i < 26; i++)
+    {
+
+	if(count[i] != 0)
+	{
+	    min_count = min(min_count, count[i]);
+	    types_of_chars++;
+	}
+    }
+
+    if(types_of_chars <= 1 || types_of_chars > 4)
+    {
+	cout << "No" << endl;
+	return(0);
+    }
+
+    if(types_of_chars >= 3 || min_count >= 2)
+    {
+	cout << "Yes" << endl;
+	return(0);
+    }
+    cout << "No" << endl;
     return(0);
 }
