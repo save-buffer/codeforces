6f768b7b276f54d79b074068786fd57dae7313e0
save-buffer <warplane@hotmail.com>
2018-04-07 18:22:59 -0700

    tried working on ufo. failed

--- CodeJam2018/ufo.cpp
+++ CodeJam2018/ufo.cpp
@@ -12,10 +12,19 @@ int main(void)
 	double A;
 	cin >> A;
 	double theta = acos(A / sqrt(2.0)) + (M_PI * 0.25);
+	tuple<double, double, double> e1, e2, e3;
+	e1 = make_tuple(0.5 * cos(theta), 0.5 * sin(theta), 0.0);
+	e2 = make_tuple(-0.5 * sin(theta), 0.5 * cos(theta), 0.0);
+	e3 = make_tuple(0.0, 0.0, 0.5);
+    	if(A <= 1.414213 + 1e-6)
+	{
+	    //find closed form for larger case
+	}
 	cout << "Case #" << z + 1 << ":" << endl;
-	cout << 0.5 * cos(theta) << ' ' << 0.5 * sin(theta) << ' ' << 0.0 << endl;
-	cout << -0.5 * sin(theta) << ' ' << 0.5 * cos(theta) << ' ' << 0.0 << endl;
-	cout << 0.0 << ' ' << 0.0 << ' ' << 0.5 << endl;
+	cout << get<0>(e1) << ' ' << get<1>(e1) << ' ' << get<2>(e1) << endl;
+	cout << get<0>(e2) << ' ' << get<1>(e2) << ' ' << get<2>(e2) << endl;
+	cout << get<0>(e3) << ' ' << get<1>(e3) << ' ' << get<2>(e3) << endl;
+
     }
     return(0);
 }
